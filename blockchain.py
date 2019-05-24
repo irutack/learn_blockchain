d3ad5303eb468fd777c89cd00600f353cdc83380
irutack <irutack+github@gmail.com>
2019-05-24 11:42:15 +0000

    Blockchainのひな型

---
+++ blockchain.py
@@ -0,0 +1,18 @@
+class BlockChain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transaction = []
+
+    def new_block(self):
+        pass
+
+    def new_transaction(self):
+        pass
+
+    @staticmethod
+    def hash(block):
+        pass
+
+    @property
+    def last_block(self):
+        pass
